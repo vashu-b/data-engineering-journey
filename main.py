df835809447779fb5a7b00d6e29543373a3678f4
vashu-b <bhagat.vashu315@gmail.com>
2026-04-29 14:15:32 +0530

    added README and modular pipeline

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print("Hello Data Engineering")
+import pandas as pd
+import mysql.connector
+
+def extract():
+    try:
+        df = pd.read_csv("sales.csv")
+        print("Data extracted")
+        return df
+    except Exception as e:
+        print("Error in extraction:", e)
+
+def transform(df):
+    try:
+        df["revenue"] = df["revenue"] * 1.1
+        print("Data transformed")
+        return df
+    except Exception as e:
+        print("Error in transformation:", e)
+
+def load(df):
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="vashup",
+            database="ecommerce_analytics"
+        )
+        cursor = conn.cursor()
+
+        for _, row in df.iterrows():
+            cursor.execute(
+                "INSERT INTO customers (first_name, revenue) VALUES (%s, %s)",
+                (row["name"], row["revenue"])
+            )
+
+        conn.commit()
+        print("Data loaded into MySQL")
+
+    except Exception as e:
+        print("Error in loading:", e)
+
+def main():
+    df = extract()
+    df = transform(df)
+    load(df)
+
+if __name__ == "__main__":
+    main()
